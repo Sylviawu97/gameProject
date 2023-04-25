9fffe624fa97dd8fb852011566daa665378403a4
Shubo Fan <shubo.fan@warnermedia.com>
2023-04-24 22:57:00 -0700

    deal card

---
+++ card-game.js
@@ -0,0 +1,109 @@
+let round = 1;
+let playerAScore = 0;
+let playerBScore = 0;
+
+// 卡牌游戏对象
+const game = {
+    // 玩家A和B
+    playerA: {
+        cards: [], // 玩家的牌组
+        score: 0, // 玩家的分数
+        name: 'Player A', // 玩家的名字
+    },
+    playerB: {
+        cards: [],
+        score: 0,
+        name: 'Player B',
+    }}
+    // 可以选择的牌
+  const dealCards=()=> {
+
+      let playerACards = []
+      let playerBCards = []
+      for (let i=0; i<3; i++) {
+          playerACards.push(Math.floor(Math.random() * 10) + 1)
+          playerBCards.push(Math.floor(Math.random() * 10) + 1)
+      }
+// 为玩家 A 创建卡牌元素并添加到容器中
+      const playerACardsContainer = document.querySelector('.player-a-cards');
+      playerACardsContainer.innerHTML = '';
+      playerACards.forEach(card => {
+          console.log(`${card}.png`)
+              const cardElement = document.createElement('img');
+              cardElement.classList.add('card');
+              cardElement.src = `${card}.png`;
+              playerACardsContainer.appendChild(cardElement);
+          }
+      )
+
+// 为玩家 B 创建卡牌元素并添加到容器中
+      const playerBCardsContainer = document.querySelector('.player-b-cards');
+      playerBCardsContainer.innerHTML = '';
+      playerBCards.forEach(card => {
+          const cardElement = document.createElement('img');
+          cardElement.classList.add('card');
+          cardElement.src = `${card}.png`;
+          playerBCardsContainer.appendChild(cardElement);
+      })
+       let resultContainer=document.getElementById("result");
+      const winner = compareCards(playerACards, playerBCards);
+      if (winner === 'A') {
+          console.log("Player A wins!")
+          resultContainer.textContent = 'Player A wins!';
+          playerAScore++;
+      } else if (winner === 'B') {
+          console.log("Player B wins!")
+          resultContainer.textContent = 'Player B wins!';
+          playerBScore++;
+      } else {
+          console.log("It is a tie")
+          resultContainer.textContent = 'Play Again!';
+      }
+
+      if (playerAScore === 5) {
+          resultContainer.textContent = 'Player A wins the game!';
+          endGame();
+      } else if (playerBScore === 5) {
+          resultContainer.textContent = 'Player B wins the game!';
+          endGame();
+      }
+  }
+
+
+
+
+
+// 比较两个牌组的大小
+function compareCards(cardsA, cardsB) {
+    if (isBomb(cardsA) && isBomb(cardsB)) {
+        return Math.max(...cardsA) > Math.max(...cardsB) ? 'A' : 'B';
+    } else if (isBomb(cardsA)) {
+        return 'A';
+    } else if (isBomb(cardsB)) {
+        return 'B';
+    } else if (isConsecutive(cardsA) && !isConsecutive(cardsB)) {
+        return 'A';
+    } else if (!isConsecutive(cardsA) && isConsecutive(cardsB)) {
+        return 'B';
+    } else if (isConsecutive(cardsA) && isConsecutive(cardsB)) {
+        return getSum(cardsA) > getSum(cardsB) ? 'A' : 'B';
+    } else {
+        return null;
+    }
+}
+
+
+function isBomb(cards) {
+    return cards[0] === cards[1] && cards[1] === cards[2];
+}
+
+function isConsecutive(cards) {
+    const sorted = cards.sort();
+    return sorted[0] + 1 === sorted[1] && sorted[1] + 1 === sorted[2];
+}
+
+function getSum(cards) {
+    return cards.reduce((sum, card) => sum + card, 0);
+}
+let startGame= document.getElementById("deal-btn");
+startGame.addEventListener('click',dealCards)
--- game.js
+++ game.js
@@ -1,20 +1,36 @@
 
-
 let rules= document.getElementById("rule");
 let rulesText=document.getElementById("rules-modal");
 let closeIntro=document.getElementById("close");
 
 rules.addEventListener("click",function(){
+
     rulesText.style.display="block";
+
 })
 
 closeIntro.addEventListener("click",function(){
     rulesText.style.display="none";
 })
- 
+
 let startGame=document.getElementsById("yes");
 let gameContent=document.getElementsByClassName("game");
 
-startGame.addEventListener("click",fuction(){
+startGame.addEventListener("click",function(){
     gameContent.style.display="block";
 })
+
+let startGame=document.getElementById("yes");
+let gameContent=document.getElementsByClassName("game-container").item(0);
+// console.log(gameContent);
+startGame.addEventListener("click",function() {
+    window.location.href ='card-game.html';
+})
+
+
+
+
+
+
+
+>>>>>>> e2ae35e (deal card)
